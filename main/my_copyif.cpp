9a9b1ceb8b4b2dcaa7ae1db6bf8bfe29b817b0ab
user <exitstop@list.ru>
2019-03-20 18:34:47 +0300

    delete std::accumulate from my_copyif.cpp

--- main/my_copyif.cpp
+++ main/my_copyif.cpp
@@ -5,7 +5,6 @@
 bool myCopyIf(std::vector<uint32_t> &v)
 {
     using namespace std;
-    size_t sumStart = std::accumulate(v.begin(), v.end(), 1, [](int x, int y){ return x + y;} );
 
     uint32_t count = 0;
     std::copy_if(v.begin(), v.end(), v.begin(), [&count](uint32_t i)
@@ -19,7 +18,6 @@ bool myCopyIf(std::vector<uint32_t> &v)
                 }
             );
     std::fill(v.begin() + count, v.end(), 0);
-    size_t sumEnd = std::accumulate(v.begin(), v.end(), 1, [](int x, int y){ return x + y;} );
     DBG(v); 
     cout << endl;
 }
