3195982bd6d790b06a12bf2c8793ac352ac7c742
user <exitstop@list.ru>
2019-04-14 23:45:02 +0300

    add file my_copyif.hpp

---
+++ main/my_copyif.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <vector>
+#include <cstdint>
+
+// функция смещает все числа == 0 в конец массива, а все числа != 0 в начало
+// и не изменяет их порядок
+void myCopyIf(std::vector<uint32_t> &v);
